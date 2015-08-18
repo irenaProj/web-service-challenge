366ea8ce49629c208e0944ca2c4c89e0e61fdff7
irenaproj <irena.shemesh@gmail.com>
2015-08-18 07:08:32 +0000

    Add body-parser to process JSON in the request

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express        = require( 'express' );
+var bodyParser     = require("body-parser");
 var http           = require( 'http' );
-
 var app            = express();
+
 app.set( 'port', process.env.PORT || 3001 );
-app.get('/', function (req, res) {
-    res.send('Hello World');
+
+// Parse application/json
+app.use(bodyParser.json())
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send(JSON.stringify(req.body, null, 2));
 });
 
 http.createServer( app ).listen( app.get( 'port' ), function (){
